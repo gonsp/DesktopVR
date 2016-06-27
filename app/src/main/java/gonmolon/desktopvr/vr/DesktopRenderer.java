0aa99cef133f96acb26a4b138c2f6d6a810f6902
Gonzalo Solera <gonzalo1796@gmail.com>
2016-06-27 18:09:00 +0200

    Gaze pointer improved

--- app/src/main/java/gonmolon/desktopvr/vr/DesktopRenderer.java
+++ app/src/main/java/gonmolon/desktopvr/vr/DesktopRenderer.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.util.Log;
 import android.view.MotionEvent;
 
+import com.google.vr.sdk.base.Eye;
 import com.google.vr.sdk.base.HeadTransform;
 
 import org.rajawali3d.lights.DirectionalLight;
@@ -23,6 +24,7 @@ public class DesktopRenderer extends VRRenderer {
 
     public Vector3 position;
     public Vector3 leftEyePos;
+    public Vector3 rightEyePos;
 
     public DesktopRenderer(Context context) {
         super(context);
@@ -30,8 +32,9 @@ public class DesktopRenderer extends VRRenderer {
 
     @Override
     protected void initScene() {
-        leftEyePos = new Vector3(0, 0, 0);
         position = new Vector3(0, 0, 0);
+        leftEyePos = new Vector3(position);
+        rightEyePos = new Vector3(position);
 
         getCurrentCamera().setFarPlane(1000);
 
@@ -42,7 +45,7 @@ public class DesktopRenderer extends VRRenderer {
         FloorGenerator.generate(this);
         pointer = new GazePointer(this);
         window = new Window(this, 8, 5f);
-        window.setPosition(0, 0, -5);
+        window.setPosition(0, 0, -10);
 
         bola = new Sphere(1f, 12, 12);
         Material material = new Material();
@@ -71,7 +74,8 @@ public class DesktopRenderer extends VRRenderer {
         cameraDir.setAll(0, 0, 1);
         cameraDir.transform(mHeadViewQuaternion);
         cameraDir.inverse();
-        return cameraDir;*/
+        return cameraDir;
+        */
         Vector3 cameraDir = new Vector3(pointer.getPosition());
         cameraDir.subtract(position);
         return cameraDir;
@@ -80,10 +84,9 @@ public class DesktopRenderer extends VRRenderer {
     @Override
     public void onNewFrame(HeadTransform headTransform) {
         super.onNewFrame(headTransform);
-        position = new Vector3(getCurrentCamera().getPosition());
+        position = new Vector3(rightEyePos);
         position.add(leftEyePos);
         position.divide(2.0f);
-        Log.d("HELLOU", position.x + ", " + position.y + ", " + position.z);
         if(pointer != null) {
             pointer.refresh();
             pointer.setClickable(window.isLookingAt());
@@ -91,9 +94,19 @@ public class DesktopRenderer extends VRRenderer {
     }
 
     @Override
+    public void onDrawEye(Eye eye) {
+        super.onDrawEye(eye);
+        if(eye.getType() == 1) {
+            leftEyePos = getCurrentCamera().getPosition();
+        } else {
+            rightEyePos = getCurrentCamera().getPosition();
+        }
+    }
+
+    @Override
     public void onRender(long elapsedTime, double deltaTime) {
+        //getCurrentCamera().setPosition(position);
         super.onRender(elapsedTime, deltaTime);
-        leftEyePos = new Vector3(getCurrentCamera().getPosition());
     }
 
 
--- app/src/main/java/gonmolon/desktopvr/vr/GazePointer.java
+++ app/src/main/java/gonmolon/desktopvr/vr/GazePointer.java
@@ -5,17 +5,18 @@ import android.opengl.Matrix;
 
 import org.rajawali3d.materials.Material;
 import org.rajawali3d.math.Matrix4;
+import org.rajawali3d.math.vector.Vector3;
 import org.rajawali3d.primitives.Sphere;
 
 public class GazePointer extends Sphere {
 
     private DesktopRenderer renderer;
     private float[] absolutePos = new float[4];
-    private float[] relativePos = {0, 0, -2, 1.0f};
+    private final float[] relativePos = new float[]{0, 0, -3f, 1.0f};
     private float[] headViewMatrix = new float[16];
 
     public GazePointer(DesktopRenderer renderer) {
-        super(0.005f, 12, 12);
+        super(0.01f, 12, 12);
         this.renderer = renderer;
         Material material = new Material();
         material.setColor(Color.RED);
@@ -28,11 +29,12 @@ public class GazePointer extends Sphere {
         matrix.setAll(renderer.mHeadViewMatrix);
         matrix.inverse().toFloatArray(headViewMatrix);
         Matrix.multiplyMV(absolutePos, 0, headViewMatrix, 0, relativePos, 0);
-        setPosition(absolutePos[0], absolutePos[1], absolutePos[2]);
-        setLookAt(renderer.getCurrentCamera().getPosition());
+        Vector3 pos = new Vector3(absolutePos[0], absolutePos[1], absolutePos[2]);
+        pos.add(renderer.position);
+        setPosition(pos);
     }
 
     public void setClickable(boolean clickable) {
-        setScale(clickable ? 2.0f : 1.0f);
+        setScale(clickable ? 1.7f : 1.0f);
     }
 }
