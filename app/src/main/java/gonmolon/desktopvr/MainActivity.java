d7acfa685d17c4aac8f383656bda8a63f5e94f46
Gonzalo Solera <gonzalo1796@gmail.com>
2016-08-12 16:15:45 +0200

    Fixing brightness

--- app/src/main/java/gonmolon/desktopvr/MainActivity.java
+++ app/src/main/java/gonmolon/desktopvr/MainActivity.java
@@ -1,6 +1,12 @@
 package gonmolon.desktopvr;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Window;
 
 import com.google.vr.sdk.base.GvrActivity;
 import com.google.vr.sdk.base.GvrView;
@@ -9,6 +15,7 @@ import gonmolon.desktopvr.vr.DesktopRenderer;
 
 public class MainActivity extends GvrActivity {
 
+    private int brightness;
     private GvrView vrView;
     private DesktopRenderer renderer;
 
@@ -16,6 +23,19 @@ public class MainActivity extends GvrActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ContentResolver contentResolver = getContentResolver();
+        try {
+            Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+            brightness = Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS);
+            Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, 255);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+            intent.setData(Uri.parse("package:" + this.getPackageName()));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
 
         vrView = new GvrView(this);
         renderer = new DesktopRenderer(this);
@@ -32,6 +52,9 @@ public class MainActivity extends GvrActivity {
 
     @Override
     protected void onPause() {
+        ContentResolver contentResolver = getContentResolver();
+        Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
+        Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
         super.onPause();
     }
 
